9c6896b14653d790e3585a73276fbd2b2244841c
Jeremy <jerekith2@gmail.com>
2015-09-26 17:11:57 +0300

    changed '/' route seponse

--- routes/api.js
+++ routes/api.js
@@ -2,13 +2,8 @@ var Character = require('../models/characters');
 var express = require('express');
 var router = express.Router();
 
-router.use(function(req, res, next) {
-  console.log('Something is happening.');
-  next();
-});
-
 router.get('/', function(req, res) {
-    res.json('Welcome to our api!');   
+    res.send('Welcome to our api!');   
 });
 
 router.get('/characters', function(req, res) {
